842079dbc34765113b53de6a4d637b2e8ff4af32
varunkamanibosc <varun.kamani@bosctechlabs.com>
2022-02-23 13:15:42 +0530

    Remove yarn Port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Configuration
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Logging
 app.use(morgan('dev'));
